f7abcd13fb9449ccd783e1e952959e4f2a1067d1
nuonuo <13403893904@163.com>
2019-09-10 18:29:32 +0800

    1230

--- pages/my/components/mySeller/mySeller.js
+++ pages/my/components/mySeller/mySeller.js
@@ -12,7 +12,10 @@ Component({
   },
   lifetimes: {
     // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
-    attached: function() {},
+    attached: function() {
+      this.drawProgressbg();
+      this.drawCircle(1)
+    },
     moved: function() {},
     detached: function() {},
   },
@@ -34,13 +37,40 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    progress_txt:"50%"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    drawProgressbg: function() {
+      // 使用 wx.createContext 获取绘图上下文 context
+      var ctx = wx.createCanvasContext('canvasProgressbg', this)
+      ctx.setLineWidth(4); // 设置圆环的宽度
+      ctx.setStrokeStyle('#999'); // 设置圆环的颜色
+      ctx.setLineCap('round') // 设置圆环端点的形状
+      ctx.beginPath(); //开始一个新的路径
+      ctx.arc(22.5, 22.5, 18.5, 0, 2 * Math.PI, false);
+      //设置一个原点(22.5,22.5)，半径为100的圆的路径到当前路径
+      ctx.stroke(); //对当前路径进行描边
+      ctx.draw();
+    },
+    drawCircle: function(step) {
+      var context = wx.createCanvasContext('canvasProgress', this);
+      // 设置渐变
+      // var gradient = context.createLinearGradient(200, 100, 100, 200);
+      // gradient.addColorStop("0", "#2661DD");
+      // gradient.addColorStop("0.5", "#40ED94");
+      // gradient.addColorStop("1.0", "#5956CC");
+      context.setLineWidth(4);
+      context.setStrokeStyle("#d5a260");
+      context.setLineCap('round')
+      context.beginPath();
+      // 参数step 为绘制的圆环周长，从0到2为一周 。 -Math.PI / 2 将起始角设在12点钟位置 ，结束角 通过改变 step 的值确定
+      context.arc(22.5, 22.5, 18.5, -Math.PI / 2, step * Math.PI - Math.PI / 2, false);
+      context.stroke();
+      context.draw()
+    }
   }
 })
